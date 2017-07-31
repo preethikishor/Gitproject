be85a75ddac6e36c8f6aadaf809cba4f4a72dd84
preethikishor <preethidj@gmail.com>
2017-08-01 01:23:34 +0530

    removed

--- Gitproject/src/Github.java
+++ Gitproject/src/Github.java
@@ -1,12 +1,11 @@
 
 public class Github {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("GITHUB ONE");
 System.out.println("GITHUB TWO");
-System.out.println("GITHUB THREE");
-System.out.println("GITHUB FOUR");
+
 	}
 
 }
