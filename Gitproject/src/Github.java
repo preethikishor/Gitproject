875d16ca03f6d4fca19fbfed571eae8eefeeab1c
preethikishor <30550110+preethikishor@users.noreply.github.com>
2020-06-26 23:22:30 +0530

    Update Github.java

--- Gitproject/src/Github.java
+++ Gitproject/src/Github.java
@@ -3,9 +3,7 @@ public class Github {
 public static void main(String[] args) {
 
 System.out.println("GITHUB Three");
-System.out.println("GITHUB four");
-System.out.println("GITHUB four");
- 
+
 }
 }
 
