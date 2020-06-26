7e6ead57011ff3c36ea3da2332526b1379265be2
preethikishor <preethidj@gmail.com>
2020-06-26 06:20:40 +0530

    dfdf

--- Gitproject/src/Github.java
+++ Gitproject/src/Github.java
@@ -3,8 +3,5 @@ public class Github {
 public static void main(String[] args) {
 System.out.println("GITHUB ONE");
 System.out.println("GITHUB TWO");
-System.out.println("GITHUB THREE");
-System.out.println("GITHUB FOUR");
-System.out.println("GITHUB SIXa");
 }
 }
